9de4df93eb1b26d34f0fd5915e5f0d7cf7b71a78
akarmanoff <arseniy.karmanov@mail.ru>
2022-10-25 10:55:21 +0700

    Выполнил задание 1. Вериант 4. Построить граф по матрицу инцидентности.

---
+++ issue-1/dm_1.py
@@ -0,0 +1,37 @@
+import matplotlib
+import networkx as nx
+import matplotlib.pyplot as plt
+import csv
+
+matplotlib.use('TkAgg')
+
+
+def matrix_to_graph(mx: list) -> nx.Graph():
+    """
+    Генерирует граф по матрице инцидентности.
+    :param mx: Матрица инцидентности.
+    :return: Сгенерированный граф по матрице инцидентности.
+    """
+    graph = nx.Graph()
+    edges = []
+    for i, line in enumerate(mx, start=1):
+        for j, number in enumerate(line, start=1):
+            i_number = int(number)
+            if i_number == 1:
+                edges.append((i, j))
+        i_line = map(int, line)
+        if sum(i_line) == 0:
+            graph.add_node(i)
+    graph.add_edges_from(edges)
+    return graph
+
+
+with open('matrix.csv', 'r') as f:
+    reader = csv.reader(f)
+    matrix = list(reader)
+
+g = matrix_to_graph(matrix)
+pos = nx.spring_layout(g)
+
+nx.draw(g, with_labels=True, pos=pos)
+plt.show()
