61e9c03d63bbc2e3ea6dfb9bc0da9b458d809dca
akarmanoff <arseniy.karmanov@mail.ru>
2022-11-06 16:15:35 +0700

    Добавил взвешенный граф.

--- issue_2/dm_2.py
+++ issue_2/dm_2.py
@@ -13,20 +13,16 @@ G.add_edge("a", "d", weight=0.3)
 elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] > 0.5]
 esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] <= 0.5]
 
-pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproducibility
+pos = nx.spring_layout(G)
 
-# nodes
 nx.draw_networkx_nodes(G, pos, node_size=700)
 
-# edges
 nx.draw_networkx_edges(G, pos, edgelist=elarge, width=6)
 nx.draw_networkx_edges(
     G, pos, edgelist=esmall, width=6, alpha=0.5, edge_color="b", style="dashed"
 )
 
-# node labels
 nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
-# edge weight labels
 edge_labels = nx.get_edge_attributes(G, "weight")
 nx.draw_networkx_edge_labels(G, pos, edge_labels)
 
